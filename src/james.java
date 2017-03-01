8f4638554d8cd404d4b0b6b0e79557bffa3205c7
afeniaguilar <league@Mac-mini-2.local>
2017-02-28 16:54:28 -0800

    we made a spiral

---
+++ src/james.java
@@ -0,0 +1,80 @@
+import org.jointheleague.graphical.robot.Robot;
+
+public class james {
+	public static void main(String[] args) {
+		Robot zuez=new Robot();
+		zuez.setSpeed(10);
+		zuez.penDown();
+		zuez.setRandomPenColor();
+		zuez.move(210);
+		zuez.turn(100);
+		zuez.move(210);
+		zuez.move(210);
+		zuez.turn(100);
+		zuez.move(210);
+		zuez.move(210);
+		zuez.turn(100);
+		zuez.move(210);
+		zuez.move(210);
+		zuez.turn(100);
+		zuez.move(210);
+		zuez.move(210);
+		zuez.turn(100);
+		zuez.move(210);
+		zuez.move(210);
+		zuez.turn(100);
+		zuez.move(210);
+		zuez.move(210);
+		zuez.turn(100);
+		zuez.move(210);
+		zuez.move(210);
+		zuez.turn(100);
+		zuez.move(210);
+		zuez.move(210);
+		zuez.turn(100);
+		zuez.move(210);
+		zuez.move(210);
+		zuez.turn(100);
+		zuez.move(210);
+		zuez.move(210);
+		zuez.turn(100);
+		zuez.move(210);
+		zuez.move(210);
+		zuez.turn(100);
+		zuez.move(210);
+		zuez.move(210);
+		zuez.turn(100);
+		zuez.move(210);
+        zuez.turn(100);
+		zuez.move(210);
+		zuez.turn(100);
+		zuez.move(210);
+		zuez.move(210);
+		zuez.turn(100);
+		zuez.move(210);
+		zuez.move(210);
+		zuez.turn(100);
+		zuez.move(210);
+		zuez.move(210);
+		zuez.turn(100);
+		zuez.move(210);
+		zuez.move(210);
+		zuez.turn(100);
+		zuez.move(210);
+		zuez.move(210);
+		zuez.turn(100);
+		zuez.move(210);
+		zuez.move(210);
+		zuez.turn(100);
+		zuez.move(210);
+		zuez.move(210);
+		zuez.turn(100);
+		zuez.move(210);
+
+
+
+	}
+
+}
+  
+
